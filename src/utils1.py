3b4b9973c3d58fff65e0efb90943b34200f764cf
rawfin <urawfin@gmail.com>
2026-05-01 19:26:23 +0600

    Add basic error handling to the calculator

--- src/utils1.py
+++ src/utils1.py
@@ -1,4 +1,7 @@
 def multiply(a,b):
     return a*b
 def divide(a,b):
-    return a/b 
+    try:
+        return a/b
+    except ZeroDivisionError:
+        return "Error : Cannot be divided by zero."
